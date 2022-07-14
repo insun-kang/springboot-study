8516cb5c05c808d8181a6839e7f3ab6a749a2846
insun <sunink1@naver.com>
2022-07-14 17:39:03 +0900

    order

--- src/main/java/jpabook/japshop/domain/item/Item.java
+++ src/main/java/jpabook/japshop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.japshop.domain.item;
 
 import jpabook.japshop.domain.Category;
+import jpabook.japshop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,12 +19,28 @@ public class Item {
     private Long id;
 
     private String name;
-
     private int price;
-
     private int stockQuntity;
+
     @ManyToMany(mappedBy = "items")
     private List<Category> catagories = new ArrayList<>();
 
+    //비즈니스 로직//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuntity += quantity;
+    }
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuntity - quantity;
 
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuntity = restStock;
+    }
 }
---
+++ src/main/java/jpabook/japshop/repository/OrderRepository.java
@@ -0,0 +1,4 @@
+package jpabook.japshop.repository;
+
+public class OrderRepository {
+}
